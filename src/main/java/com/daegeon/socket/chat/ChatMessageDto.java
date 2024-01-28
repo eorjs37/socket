562751bda77f4ed0708e838f9ba532fe832b9ca8
macdaegeon <chleorjs37@gmail.com>
2024-01-28 19:02:11 +0900

    소켓 통신 진행

---
+++ src/main/java/com/daegeon/socket/chat/ChatMessageDto.java
@@ -0,0 +1,21 @@
+package com.daegeon.socket.chat;
+
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatMessageDto {
+    //메시지 타입 : 입장,채팅
+    public enum MessageType{
+        ENTER,
+        TALK
+    }
+
+    private MessageType messageType;
+    private Long chatRoomId;
+    private Long senderId;
+    private String message;
+}
